d3352e14636faf25da479c28f6b0fd45e4ba23db
bmlegend <bmdeveloper@users.noreply.github.com>
2015-04-21 22:15:05 +0100

    Initialise mars rover location

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-﻿
+﻿function MarsRover(location) {
+    var that = (this === window) ? {} : this;
+
+    //initialise location
+    that.location = location;
+
+}
--- tests.js
+++ tests.js
@@ -1,1 +1,5 @@
-﻿
+﻿QUnit.module("Implement commands that move the rover forward/backward");
+QUnit.test("Initialise mars rover location", function (assert) {
+    var marsRover = new MarsRover([3, 8]);
+    assert.deepEqual(marsRover.location, [3, 8], "Mars Rover location should be set to the [3,8] set value");
+});
