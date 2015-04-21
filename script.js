f095d8e70dc435060389ce2cab64cadb448842b0
bmlegend <bmdeveloper@users.noreply.github.com>
2015-04-21 22:33:51 +0100

    check forward movement facing North

--- script.js
+++ script.js
@@ -14,4 +14,24 @@
     else {
         that.direction = direction;
     }
+    //function that takes commands as an input
+    that.commandsInput = function (commands) {
+        //loop through commands
+        for (var i = 0; i < commands.length; i++) {
+            move(commands[i]);
+        }
+    }
+    function move(command) {
+        var yIndexChange;
+        switch (that.direction) {
+            case "N":
+                yIndexChange = 1;
+                break;
+        }
+        var newLocation = [that.location[0],that.location[1]+yIndexChange];
+        that.location = newLocation;
+    }
+
+
+
 }
--- tests.js
+++ tests.js
@@ -11,8 +11,12 @@ QUnit.test("Initialise mars rover direction", function (assert) {
     var marsRover = new MarsRover([0, 0], "S");
     assert.equal(marsRover.direction, "S", "Mars Rover direction should be the set value S");
 });
-QUnit.test("Initialise mars rover direction when it has no assigned values", function (assert) {
+QUnit.test("Initialise mars rover direction when it has no assigned value", function (assert) {
     var marsRover = new MarsRover([0, 0]);
     assert.equal(marsRover.direction, "N", "Mars Rover direction should initialise to N if no value has been set");
 });
-
+QUnit.test("Check forward movement when facing North", function (assert) {
+    var marsRover = new MarsRover([0, 0], "N");
+    marsRover.commandsInput("f");
+    assert.deepEqual(marsRover.location, [0, 1], "Mars Rover location should move to [0,1]");
+});
